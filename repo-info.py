a8d58880870ebd4bcee838c306eaa694feae41e6
MJ410 <jay.mehta@bricksyard.com>
2021-09-20 18:25:22 +0530

    first

---
+++ repo-info.py
@@ -0,0 +1,15 @@
+# importing the requests library
+import requests
+# defining the api-endpoint
+API_ENDPOINT = "http://f337-115-119-250-30.ngrok.io/train"
+# data to be sent to api
+data = {
+	"url": "https://github.com/MJ410/MLops-test/tree/master/Proj_1",
+	"branch_name": "master",
+	"algo_type": "Regression"
+}
+# sending post request and saving response as response object
+r = requests.post(url = API_ENDPOINT, data = data)
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
