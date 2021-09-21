048e709c604e813cce905cd964e1677901aa8df9
MJ410 <jay.mehta@bricksyard.com>
2021-09-21 11:31:50 +0530

    sixth

--- repo-info.py
+++ repo-info.py
@@ -1,7 +1,7 @@
 # importing the requests library
 import requests
 # defining the api-endpoint
-API_ENDPOINT = "http://1b43-115-119-250-30.ngrok.io/train"
+API_ENDPOINT = "http://2510-115-119-250-30.ngrok.io/train"
 # data to be sent to api
 data = {
 	"url": "https://github.com/MJ410/test-mlops",
